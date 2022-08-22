e358bf86408c8f404ac36d703ee838c99526dc0a
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2022-08-22 21:42:11 +0100

    updates

---
+++ index.js
@@ -0,0 +1,32 @@
+let icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src = "images/sun (1).png";
+    } else {
+        icon.src = "images/night-mode.png";
+    }
+}
+
+ let name = document.getElementById("fname");
+ let lname = document.getElementById("lname")
+ let password = document.getElementById("password");
+ let confirm = document.getElementById("confirm");
+ let form = document.getElementById("form");
+
+function passValid(){
+    let password = document.getElementById("password");
+    let confirm = document.getElementById("confirm");
+
+    if(password != confirm) {
+        alert("Passwords do not match");
+    }
+    else {
+        alert("Pasword created successfully");
+    }
+}
+
+const toggle = document.getElementById("toggle");
+toggle.onclick = function() {
+    toggle.classList.toggle("active")
+}
