641c25b315a26cc5a7491eef9f1b19c51bbb7c2a
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2022-08-25 01:05:24 +0100

    updates

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 let icon = document.getElementById("icon");
-icon.onclick = function(){
-    document.body.classList.toggle("light-theme");
-    if(document.body.classList.contains("light-theme")){
-        icon.src = "images/night-mode.png";
-    } else {
-        icon.src = "images/sun (1).png";
-    }
-}
+icon.onclick = function () {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
+    icon.src = "images/night-mode.png";
+  } else {
+    icon.src = "images/sun (1).png";
+  }
+};
 
- let name = document.getElementById("fname");
- let lname = document.getElementById("lname")
- let password = document.getElementById("password");
- let confirm = document.getElementById("confirm");
- let form = document.getElementById("form");
+let name = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let password = document.getElementById("password");
+let confirm = document.getElementById("confirm");
+let form = document.getElementById("form");
 
-function passValid(){
-    let password = document.getElementById("password");
-    let confirm = document.getElementById("confirm");
+function passValid() {
+  let password = document.getElementById("password");
+  let confirm = document.getElementById("confirm");
 
-    if(password != confirm) {
-        alert("Passwords do not match");
-    }
-    else {
-        alert("Pasword created successfully");
-    }
+  if (password != confirm) {
+    alert("Passwords do not match");
+  } else {
+    alert("Pasword created successfully");
+  }
 }
 
 const toggle = document.getElementById("toggle");
-toggle.onclick = function() {
-    toggle.classList.toggle("active")
-}
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+};
